0d631b89658091446eafd6e957fb3a16ec8ad90e
Khusan <khusan7700@gmail.com>
2025-07-21 22:05:52 +0900

    fix: some bugs

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 # PRODUCTION
 git reset --hard
+git checkout master
 git pull origin master
 
 npm i npm -g
-npm global add serve
-npm
+npm install -g serve
+npm install
 npm run build
-pm2 start "npm run start" --name=kakao
+pm2 start "npm run start" --name=KAKAO-NEXT
